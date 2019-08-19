f6d004bca08719409a3e711ce9124f41e894f9eb
Roy Wakumelo Jr <roywakumelo@roys-mbp.attlocal.net>
2019-08-19 18:08:19 -0400

    MVP and Stretch has been reached

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -45,3 +45,8 @@ subtract(1,2);
 //   return num * 3;
 // });
 // console.log(triple);
+exampleArray = [1,2,3,4];
+const triple = exampleArray.map( (num) => {
+  return num * 3;
+});
+console.log(triple);
